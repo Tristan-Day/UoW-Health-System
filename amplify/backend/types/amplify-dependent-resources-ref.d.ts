448056bee0bc1109bc7b3e841d81e3f64fe114db
Tristan Day <tristan.day.uk@gmail.com>
2024-01-11 19:37:08 +0000

    Fix merge issues on PatientFunction

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,5 +1,20 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
+    "CleaningOrderHandler": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    },
+    "PermissionHandler": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    },
+    "RoomHandler": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    },
     "StaffHandler": {
       "ApiId": "string",
       "ApiName": "string",
@@ -7,6 +22,26 @@ export type AmplifyDependentResourcesAttributes = {
     }
   },
   "function": {
+    "CleaningOrderFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "PermissionFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "RoomFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "StaffFunction": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
