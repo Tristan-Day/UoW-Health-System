7812245faf277c6bf87ef103f87b3f3b3058338e
Tristan Day <tristan.day.uk@gmail.com>
2024-01-12 20:26:45 +0000

    improve performance of doc agregation script

--- scripts/aggregate-docs.py
+++ scripts/aggregate-docs.py
@@ -1,4 +1,5 @@
 import os
+import threading
 import subprocess
 
 OUTPUT_FOLDER = "docs"
@@ -8,34 +9,41 @@ def process(path: str, output: str):
     if "swagger.js" not in os.listdir(path):
         print(f"Directory {path} has no swagger.js file, skipping")
         return
-    
+
     if subprocess.call("npm install", cwd=path, shell=True):
         print(f"Failed to install npm pages in '{path}'")
         return
 
-    try:
-        subprocess.call("npm run swagger", cwd=path, shell=True)
-    except:
+    if subprocess.call("npm run swagger", cwd=path, shell=True):
         print(f"Error running 'npm swagger' in '{path}'")
         return
 
     service = path.split("\\")[-2]
 
-    try:
-        subprocess.call(
-            f'swagger-markdown -i swagger-output.json -o "{output}\\{service}.md"',
-            cwd=path,
-            shell=True,
-        )
-    except:
+    if subprocess.call(
+        f'swagger-markdown -i swagger-output.json -o "{output}\\{service}.md"',
+        cwd=path,
+        shell=True,
+    ):
         print(f"Error running 'swagger-markdown' in '{path}'")
         return
 
 
 def index():
+    threads = []
+
     for obj in os.scandir(".\\amplify\\backend\\function"):
         if obj.is_dir() and "Function" in obj.name:
-            process(obj.path + "\\src", os.getcwd() + f"\\{OUTPUT_FOLDER}")
+            threads.append(
+                threading.Thread(
+                    target=lambda path, output: process(path, output),
+                    args=[obj.path + "\\src", os.getcwd() + f"\\{OUTPUT_FOLDER}"],
+                )
+            )
+
+    [thread.start() for thread in threads]
+    [thread.join() for thread in threads]
+
 
 def main():
     if "amplify" not in os.listdir("./"):
@@ -47,6 +55,11 @@ def main():
 
     if "docs" not in os.listdir("./"):
         os.mkdir(f"./{OUTPUT_FOLDER}")
+    else:
+        print("Removing existing files")
+        for obj in os.scandir(f"./{OUTPUT_FOLDER}"):
+            if obj.is_file() and ".md" in obj.name:
+                os.remove(obj.path)
 
     index()
 
