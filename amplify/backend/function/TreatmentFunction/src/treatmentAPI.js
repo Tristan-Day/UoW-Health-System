397b5004275043fe1e3a9a3e717b5229fad86924
Ben Wright <benwright9400@gmail.com>
2024-01-12 08:20:09 +0000

    Add TreatmentFunction from ward-managent branch

---
+++ amplify/backend/function/TreatmentFunction/src/treatmentAPI.js
@@ -0,0 +1,308 @@
+
+class Validator {
+
+    static fields = {
+        ACTION_TYPE: String,
+        TREATMENT_ID: Number,
+        NAME: String,
+        CATEGORY_ID: Number,
+        WARD_ID: String,
+    };
+
+    static searchIsValid = function (searchObj) {
+
+        let search = Object.keys(searchObj);
+
+        //ID
+        if (search.hasOwnProperty("TREATMENT_ID")) {
+            return true;
+        }
+
+        if (search.length === 0) {
+            return true;
+        }
+
+        //check that all fields in the query statement are correct
+        let falseElementCount = 0;
+        search.forEach(element => {
+
+            if (!Object.keys(this.fields).includes(element)) {
+                falseElementCount++;
+            }
+
+        });
+
+        if (falseElementCount == 0) {
+            return true;
+        }
+
+        return false;
+
+    };
+
+    static upsertIsValid = function (searchBody) {
+
+        let search = Object.keys(searchBody);
+
+        console.log(search);
+
+        //ID
+        if (search.hasOwnProperty("TREATMENT_ID")) {
+            return true;
+        }
+
+        if (search.length === 0) {
+            return false;
+        }
+
+        //check that all fields in the query statement are correct
+        let falseElementCount = 0;
+        search.forEach(element => {
+
+            if (!Object.keys(this.fields).includes(element)) {
+                falseElementCount++;
+            }
+
+        });
+
+        if (falseElementCount == 0) {
+            return true;
+        }
+
+        return false;
+
+    };
+
+    static deleteIsValid = function (deleteBody) {
+
+        let search = Object.keys(deleteBody);
+
+        //ID
+        if (search.hasOwnProperty("TREATMENT_ID")) {
+            return true;
+        }
+
+        if (search.length === 0) {
+            return false;
+        }
+
+        const deleteFields = ["TREATMENT_ID"];
+
+        //check that all fields in the query statement are correct
+        let falseElementCount = 0;
+        search.forEach(element => {
+
+            if (!deleteFields.includes(element)) {
+                falseElementCount++;
+            }
+
+        });
+
+        if (falseElementCount == 0) {
+            return true;
+        }
+
+        return false;
+
+    };
+
+}
+
+class TreatmentAPI {
+
+    static hasCorrectPermissions = async function () {
+        return true;
+    }
+
+    static getValueForKey = function (query, key) {
+        return Object.values(query)[Object.keys(query).indexOf(key)];
+    }
+
+    static query = async function (req, res, setup) {
+        let client = await setup();
+
+        if (!this.hasCorrectPermissions()) {
+            res.status(400).json({ failure: "INCORRECT_PERMISSIONS" });
+            return;
+        }
+
+        if (!Validator.searchIsValid(req.query)) {
+            res.status(400).json({ failure: "INCORRECT_QUERY" });
+            return;
+        }
+
+
+        let result = {};
+
+        try {
+            console.log("starting query");
+
+            let paramLength = Object.keys(req.query).length;
+
+            let query;
+
+            console.log(req.query);
+            console.log(typeof req.query["TREATMENT_ID"]);
+
+            if (Object.keys(req.query).includes("TREATMENT_ID")) {
+                console.log("selecting one by ID");
+
+                console.log(Number(req.query["TREATMENT_ID"]));
+                console.log(typeof Number(req.query["TREATMENT_ID"]));
+
+                query = await client.query('SELECT * FROM "system".treatments WHERE TREATMENT_ID = ' + Number(req.query["TREATMENT_ID"]) + ';');
+
+                result = { success: query };
+
+            }
+
+            if (paramLength == 0) {
+                console.log("selecting all");
+                query = await client.query('SELECT * FROM "system".treatments;');
+
+                result = { success: query };
+
+            }
+
+            //compound query
+            if (paramLength > 0 && !Object.keys(req.query).includes("TREATMENT_ID")) {
+                console.log("running multiple iterations");
+
+                let queryString = "SELECT * FROM \"system\".treatments WHERE ";
+                let columnsArray = Object.keys(req.query);
+                let values = Object.values(req.query);
+
+                values.forEach((value, index) => {
+                    console.log(value);
+                    console.log(typeof value);
+
+                    if (typeof value == "string") {
+                        queryString += columnsArray[index] + " LIKE '" + value + "%' ";
+                    } else {
+                        queryString += columnsArray[index] + " = " + value + " ";
+                    }
+
+                    if (index < values.length - 1) {
+                        queryString += "OR ";
+                    }
+                });
+
+                queryString += ";";
+
+                console.log(queryString);
+
+                query = await client.query(queryString);
+
+                result = { success: query };
+
+            }
+
+        } catch (error) {
+
+            console.log(error);
+
+            result = { failure: error };
+
+        }
+
+        res.json(result);
+
+    }
+
+    static upsert = async function (req, res, setup) {
+        let client = await setup();
+
+        if (!this.hasCorrectPermissions()) {
+            res.status(400).json({ failure: "INCORRECT_PERMISSIONS" });
+            return;
+        }
+
+        if (!Validator.upsertIsValid(req.body)) {
+            res.status(400).json({ failure: "INCORRECT_QUERY" });
+            return;
+        }
+
+        console.log(req.body);
+
+
+        let result = {};
+        try {
+            if (req.body["ACTION_TYPE"] === "INSERT") {
+
+                const queryString = `
+                    INSERT INTO "system".treatments (NAME, CATEGORY_ID, WARD_ID)
+                    VALUES ($1, $2, $3)
+                    `;
+                const values = [req.body["NAME"], req.body["CATEGORY_ID"], req.body["WARD_ID"]];
+
+                let query = await client.query(queryString, values);
+                result = { success: query };
+
+            }
+
+            if (req.body["ACTION_TYPE"] === "UPDATE") {
+
+                const queryString = `
+                UPDATE "system".treatments SET NAME = $1, CATEGORY = $2, WARD_ID = $3 WHERE TREATMENT_ID = $4;
+                `;
+                const values = [
+                    req.body["NAME"],
+                    req.body["CATEGORY"],
+                    req.body["WARD_ID"],
+                    //where
+                    req.body["TREATMENT_ID"]
+                ];
+
+                let query = await client.query(queryString, values);
+                result = { success: query };
+
+            }
+
+        } catch (error) {
+            console.log(error);
+            result = { failure: error };
+        }
+
+        res.json(result);
+
+    }
+
+    static delete = async function (req, res, setup) {
+        let client = await setup();
+
+        if (!this.hasCorrectPermissions()) {
+            res.status(400).json({ failure: "INCORRECT_PERMISSIONS" });
+            return;
+        }
+
+        if (!Validator.deleteIsValid(req.body)) {
+            res.status(400).json({ failure: "INCORRECT_QUERY" });
+            return;
+        }
+
+        console.log(req.body);
+
+
+        let result = {};
+        try {
+
+            const queryString = `
+                    DELETE FROM "system".treatments WHERE TREATMENT_ID = $1;
+                    `;
+            const values = [req.body["TREATMENT_ID"]];
+
+            let query = await client.query(queryString, values);
+            result = { success: query };
+
+        } catch (error) {
+            console.log(error);
+            result = { failure: error };
+        }
+
+        res.json(result);
+
+    }
+
+}
+
+module.exports = TreatmentAPI;
