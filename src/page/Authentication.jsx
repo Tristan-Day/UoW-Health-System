d8eab3aa784f79bbdaa9485950ad60b4bdedac20
Tristan Day <tristan.day.uk@gmail.com>
2024-02-22 20:40:25 +0000

    Update authentication placeholder * Update role details page

--- src/page/Authentication.jsx
+++ src/page/Authentication.jsx
@@ -1,10 +1,9 @@
-import { Typography, Stack } from '@mui/material'
-
+import { Typography, Stack, CircularProgress, Box } from '@mui/material' // Import Box component
 import { getCurrentUser } from 'aws-amplify/auth'
 import { useEffect, useState } from 'react'
 
 const Authentication = () => {
-  const [user, setUser] = useState({ username: 'Loading...' })
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     getCurrentUser()
@@ -24,15 +23,42 @@ const Authentication = () => {
         justifyContent: 'center'
       }}
     >
-      <Typography variant="h4">
-        You are not yet Authorised to Access this System
-      </Typography>
-      <Typography variant="h5">
-        Please send the following code to your system administrator
-      </Typography>
-      <Typography variant="h2" sx={{ paddingTop: '2rem' }}>
-        {user.username}
-      </Typography>
+      {user ? (
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            textAlign: 'center',
+            height: '100%'
+          }}
+        >
+          <Typography variant="h4">
+            You are not yet Authorized to Access this System
+          </Typography>
+          <Typography variant="h5">
+            Please send the following code to your system administrator
+          </Typography>
+          <Typography variant="h2" sx={{ paddingTop: '2rem' }}>
+            {user.username}
+          </Typography>
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            textAlign: 'center',
+            height: '100%'
+          }}
+        >
+          <Typography variant="h4">Checking Authorization</Typography>
+          <CircularProgress />
+        </Box>
+      )}
     </Stack>
   )
 }
