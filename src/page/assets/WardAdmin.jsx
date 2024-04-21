77aeb28ba35de4b92c5bfc0334ecd8c95bc7fddd
Ben Wright <benwright9400@gmail.com>
2024-04-21 18:11:13 +0100

    Add ward admin page structure

---
+++ src/page/assets/WardAdmin.jsx
@@ -0,0 +1,106 @@
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Divider,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography
+} from '@mui/material'
+import { BreadcrumbGenerator } from '../../components'
+import { useEffect, useState } from 'react'
+import WardsAPI from './logic/Wards'
+
+const STAFF_ASSIGNMENT = 'STAFF_ASSIGNMENT'
+const ORDERS_NOTES = 'ORDERS_NOTES'
+
+function WardAdmin() {
+  const [page, setPage] = useState(ORDERS_NOTES)
+  const [wards, setWards] = useState([])
+
+  const [selectedWard, setSelectedWard] = useState("");
+
+  useEffect(() => {
+    getWards()
+  }, [])
+
+  function getButtonStyling(buttonPage) {
+    return buttonPage === page ? 'contained' : 'outlined'
+  }
+
+  async function getWards() {
+    try {
+      const res = await WardsAPI.getWard()
+      console.log(res)
+      if (res.rows) {
+        setWards(res.rows)
+      }
+    } catch (error) {
+      console.log(error)
+      setWards([])
+    }
+  }
+
+  return (
+    <Box>
+      {/* Header */}
+      <BreadcrumbGenerator />
+      <Typography variant="h4">Ward Administration</Typography>
+
+      {/* Page navigation bar */}
+      <Box sx={{ display: 'flex', marginTop: 1 }}>
+        <Box sx={{ marginRight: 2 }}>
+          <InputLabel id="button-label">Page</InputLabel>
+          <ButtonGroup labelId="button-label" sx={{ marginTop: 0.25 }}>
+            <Button
+              variant={getButtonStyling(STAFF_ASSIGNMENT)}
+              onClick={() => setPage(STAFF_ASSIGNMENT)}
+            >
+              Staff assignment
+            </Button>
+            <Button
+              variant={getButtonStyling(ORDERS_NOTES)}
+              onClick={() => setPage(ORDERS_NOTES)}
+            >
+              Orders / Notes
+            </Button>
+          </ButtonGroup>
+        </Box>
+
+        <Box>
+          <InputLabel id="select-label">Ward</InputLabel>
+          <Select
+            id="ward-select"
+            labelId="select-label"
+            value={(wards.length > 0) && (selectedWard.length === "") ? wards[0].ward_id : selectedWard}
+            size="small"
+            placeholder='Select ward'
+            onChange={(e) => setSelectedWard(e.target.value)}
+          >
+            {wards.map((ward) => {
+              return <MenuItem value={ward.ward_id}>{ward.ward_name}</MenuItem>
+            })}
+          </Select>
+        </Box>
+      </Box>
+
+      <Divider sx={{ marginTop: '1rem', marginBottom: '1rem' }} />
+
+      {/* Body */}
+
+      {/* Staff assignment page */}
+      <Box sx={page === STAFF_ASSIGNMENT ? {} : {visibility: 'hidden', height: 0, overflow: 'hidden'}}>
+       Staff assignment
+      </Box>
+
+      {/* Order and Notes page */}
+      <Box sx={page === ORDERS_NOTES ? {} : {visibility: 'hidden', height: 0, overflow: 'hidden'}}>
+      Order and Notes
+      </Box>
+
+    </Box>
+  )
+}
+
+export default WardAdmin
