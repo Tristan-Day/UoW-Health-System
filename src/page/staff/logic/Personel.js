7745a87fc73660bc5666522c75a71fdeedeab1f4
Ben Wright <benwright9400@gmail.com>
2024-04-22 15:32:57 +0100

    Fix query parameter

--- src/page/staff/logic/Personel.js
+++ src/page/staff/logic/Personel.js
@@ -46,7 +46,7 @@ export const getStaff = async query => {
     apiName: 'StaffHandler',
     path: `/v1/resources/staff/search`,
     options: {
-      body: { query: query }
+      body: { queryStringParams: query }
     }
   })
 
